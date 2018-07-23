fd6d9c2d04b612cfbeb2e7f0109c003d17002cc4
irving2delgado <36970739+irving2delgado@users.noreply.github.com>
2018-07-23 17:02:38 -0600

    setup redux

--- warbler-client/src/store/index.js
+++ warbler-client/src/store/index.js
@@ -1,15 +1,13 @@
 import rootReducer from "./reducers";
 import { createStore, applyMiddleware, compose } from "redux"; 
 import thunk from "redux-thunk";
-import { createStore, compose, applyMiddleware } from "../../node_modules/redux";
-import rootReducer from "./reducers";
 
 export function  configureStore() {
     const store = createStore(
         rootReducer, 
         compose(
             applyMiddleware(thunk),
-            window.devToolsExtension ? windows.devToolsExtension() : f =>
+            window.devToolsExtension ? window.devToolsExtension() : f => f
         )
     );
     return store;
