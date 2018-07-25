ad8201aed29275817c584d7ada40f97d71faf5f9
irving2delgado <36970739+irving2delgado@users.noreply.github.com>
2018-07-25 03:10:55 -0600

    AuthForm setup

---
+++ warbler-client/src/components/Authform.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+
+export default class AuthForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            username: "",
+            password: "",
+            profileImageUrl: ""
+        };
+    }
+
+// This allows text to be entered into input arrow function so binding in constructor isnt nessecary
+    handleChange = (e) => {   
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    render(){
+        const { email, username, password, profileImageUrl } = this.state;
+        const { heading, buttonText, signUp } = this.props;
+        return(
+            <div>
+                <div className="row justify-content-md-center text-center">
+                <div className="col-md-6">
+                    <form onSubmit={this.handleSubmit}>
+                        <h2>{heading}</h2>
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            className="form-control"
+                            id="email"
+                            name="email"
+                            onChange={this.handleChange}
+                            value={email}
+                            type="text"
+                        />
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            className="form-control"
+                            id="password"
+                            name="password"
+                            onChange={this.handleChange}
+                            type="password"
+                        />
+                        {signUp && (                /* {(if this part is true) && (this part will execute)}  */
+                                <div>
+                                    <label htmlFor="username">Username:</label>
+                        <input
+                            className="form-control"
+                            id="username"
+                            name="username"
+                            onChange={this.handleChange}
+                            value={username}
+                            type="text"
+                        />
+                        <label htmlFor="image-url">Image URL:</label>
+                        <input
+                            className="form-control"
+                            id="image-url"
+                            name="profileImageUrl"
+                            onChange={this.handleChange}
+                            type="text"
+                            value={profileImageUrl}
+                        />
+                                </div>
+                        )}
+                    </form>
+                </div>
+                </div>
+            </div>
+        )
+    }
+}
--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -1,13 +1,34 @@
 import React from "react";
 import { Switch, Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import Homepage from "../components/Homepage"
+import Homepage from "../components/Homepage";
+import AuthForm from "../components/Authform";
 
 const Main = props => {
     return(
         <div className="container">
             <Switch>
-                <Route exact path="/" render={props=> <Homepage {...props} />} />
+                <Route exact path="/" render={props => <Homepage {...props} />} />
+                <Route exact path="/signin" 
+                render={props => {
+                    return (
+                        <AuthForm 
+                        buttonText="Log in" 
+                        heading="Welcome Back." {...props} />
+                    );
+                }} 
+                />
+                <Route exact path="/signup" 
+                render={props => {
+                    return (
+                        <AuthForm 
+                        signUp          /* pass down this prop for if then short circuit operator */
+                        buttonText="Sign Me Up!" 
+                        heading="Join Warbler Today" 
+                        {...props} />
+                    );
+                }} 
+                />
             </Switch>
         </div>    
     )
